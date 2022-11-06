7e02e7e21bf5c04870cf12094cf8521ccf09691e
Mateus <mateuslopes1717@gmail.com>
2022-11-06 13:07:32 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #aaaaff;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 24px;
+  font-family: "Roboto";
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #aaaaff;
+    border: 0;
+    font-size: 24px;
+    font-family: "Roboto";
+    padding: 0 10px;
+    color:#FFF;
+  }
+`;
